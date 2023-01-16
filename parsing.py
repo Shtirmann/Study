681e2a4421f3ce585f697a3208cb889321ffc724
HQ\PopokNA <popokna@it.mos.ru>
2023-01-16 15:04:06 +0300

    Parsing URL updated

--- parsing.py
+++ parsing.py
@@ -7,14 +7,15 @@ import requests                                                 #импорт б
 from bs4 import BeautifulSoup as b                              #импорт библиотеки BeautifulSoup в константу b (для сокращения дальнейшего кода)
 
 sign = 'libra'                                                  #значение переменной sign
-date = 'day'                                                    #значение переменной date
+date = 'year'                                                    #значение переменной date
 
-URL = (f"https://www.elle.ru/astro/{sign}/{date}/")             #Исходный URL с которого парсим, с помощью (f"...") подставляем переменные в строку
+URL = (f"https://www.marieclaire.ru/astro/{sign}/{date}/")      #Исходный URL с которого парсим, с помощью (f"...") подставляем переменные в строку
 
 def parser(URL):                                                #Функция парсинга, принимает на вход URL
     r = requests.get(URL)                                       #GET запрос к URL
     soup = b(r.text, "html.parser")                             #Выгрузка текста со страницы
-    zodiac = soup.find_all("div", class_="articleParagraph")    #Откуда выгрузка, указание блока "div" и класса "articleParagraph"
+    zodiac = soup.find_all("div", class_="block-text")    		#Откуда выгрузка, указание блока "div" и класса "articleParagraph"
     return [c.text for c in zodiac]                             #Очистка ненужного содержимого из результата парсинга
 clear_zodiac = parser(URL)                                      #Запись в переменную результатов выполнения функции парсинга
 print(clear_zodiac)                                             #Печать в консоль содержимого переменной
+input ()
