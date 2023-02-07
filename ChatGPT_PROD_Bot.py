ced15aac104bd7c74cf9ecd16f5eae5aa99a4721
HQ\PopokNA <popokna@it.mos.ru>
2023-02-07 11:59:43 +0300

    Return old version

--- ChatGPT_PROD_Bot.py
+++ ChatGPT_PROD_Bot.py
@@ -15,34 +15,35 @@ if not os.path.exists('/tmp/bot_log/'):
 
 logging.basicConfig(filename='/tmp/bot_log/log.txt', level=logging.ERROR)
 
-def generate_response(prompt):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.0
-    )
-    return response["choices"][0]["text"]
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, 'Привет!\nЯ ChatGPT Telegram Bot\U0001F916\nЗадай мне любой вопрос и я постараюсь на него ответить')
+    bot.reply_to(message, 'Привет!\nЯ ChatGPT Telegram Bot\U0001F916\nЗадай мне любой вопрос и я постараюсь на него ответиь')
 
-@bot.message_handler(func = lambda message: message.text and (message.chat.type == "group" or message.chat.type == "supergroup") and message.text.startswith("/bot "))
-def handle_command_group(message):
-	prompt = message.text.replace("/bot ", "", 1)
-	response = generate_response(prompt)
-	bot.reply_to(message, text=response)
+def generate_response(prompt):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.5,
+            presence_penalty=0.0
+        )
+        return response["choices"][0]["text"]
 
-@bot.message_handler(func = lambda message: message.text and message.chat.type == "private")
-def handle_message_private(message):
-	prompt = message.text
-	response = generate_response(prompt)
-	bot.send_message(chat_id=message.from_user.id, text=response)
 
+@bot.message_handler(commands=['bot'])
+def command_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.reply_to(message, text=response)
+    
+@bot.message_handler(func = lambda _: True)
+def handle_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.send_message(chat_id=message.from_user.id, text=response)
+    
 while True:
     try:
         bot.polling()
